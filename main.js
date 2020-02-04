8a0a759cfc9520745285fdab5c88073bb2086ec3
bongbonnggg <bongbonnggg@gmail.com>
2020-02-04 16:47:25 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+gsap.from("#monitor", { duration: 3, x: 300, opacity: 0, scale: 0.5, });
+gsap.from("#monitor1", { duration: 7, x: 300, opacity: 0, scale: 0.5, });
+gsap.from("#monitor2", { duration: 5, x: 300, opacity: 0, scale: 0.5, });
