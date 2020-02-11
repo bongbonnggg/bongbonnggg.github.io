60b7f601bd34d7d12086b0196a8532ae6f019726
bongbonnggg <bongbonnggg@gmail.com>
2020-02-11 13:24:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-gsap.from("#monitor", { duration: 3, x: 300, opacity: 0, scale: 0.5, });
-gsap.from("#monitor1", { duration: 7, x: 300, opacity: 0, scale: 0.5, });
-gsap.from("#monitor2", { duration: 5, x: 300, opacity: 0, scale: 0.5, });
+gsap.from("#monitor", { duration: 2, x: 300, opacity: 0, scale: 0.5, rotate: 360 });
+gsap.from("#monitor1", { duration: 6, x: 300, opacity: 0, scale: 0.5, rotate: 360 });
+gsap.from("#monitor2", { duration: 4, x: 300, opacity: 0, scale: 0.5, rotate: 360 });
+
+
+
+// Swiper JS - HOME
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
