6cadcb7cb69542c7ac9f823d78cd27eef8da4445
bongbonnggg <bongbonnggg@gmail.com>
2020-02-14 18:02:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ gsap.to("#css", { duration: 7, delay: 6, opacity: 1, width: 50, rotate: 360, y: 
 gsap.to("#photoshop", { duration: 6, delay: 6, opacity: 1, width: 40, rotate: 360, y: -350, repeat: -1 });
 gsap.to("#wordpress", { duration: 3, delay: 6, opacity: 1, width: 50, rotate: 360, y: -200, repeat: -1 });
 
+
+//tweens overlap
+// var tl = gsap.timeline();
+// tl.to("#green", { duration: 2, x: 750 })
+//     // insert 1 second before end of timeline
+//     .to("#blue", { duration: 2, x: 750 }, "-=1")
+//     // insert 1 second before end of timeline
+//     .to("#orange", { duration: 2, x: 750 }, "-=1");
+
 // Swiper JS - HOME
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
