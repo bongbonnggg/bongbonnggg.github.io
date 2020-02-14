a08fe763042180909421ff3752958a7e8ecba759
bongbonnggg <bongbonnggg@gmail.com>
2020-02-14 17:33:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,10 @@
 gsap.from("#monitor", { duration: 2, x: 300, opacity: 0, scale: 0.5, rotate: 360 });
 gsap.from("#monitor1", { duration: 6, x: 300, opacity: 0, scale: 0.5, rotate: 360 });
 gsap.from("#monitor2", { duration: 4, x: 300, opacity: 0, scale: 0.5, rotate: 360 });
-gsap.to("#html", { duration: 2, width: 50, rotate: 360, x: -200, y: -200, repeat: -1 });
+gsap.to("#html", { duration: 5, delay: 6, opacity: 1, width: 50, rotate: 360, y: -200, repeat: -1 });
+gsap.to("#css", { duration: 7, delay: 6, opacity: 1, width: 50, rotate: 360, y: -300, repeat: -1 });
+gsap.to("#photoshop", { duration: 6, delay: 6, opacity: 1, width: 40, rotate: 360, y: -350, repeat: -1 });
+gsap.to("#wordpress", { duration: 3, delay: 6, opacity: 1, width: 50, rotate: 360, y: -200, repeat: -1 });
 
 // Swiper JS - HOME
 var swiper = new Swiper('.swiper-container', {
